0b4638dd8b8f9dc2f4d7dca2b32e94a7b022c4e9
volpicelli <ca.volpicelli@gmail.com>
2025-05-06 20:07:31 +0200

    Workiing on Utenti for Aziende

--- access/urls.py
+++ access/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import Login ,Authenticate,SelectAzienda,Logout#,GetCantieri
+from .views import Login,Authenticate,SelectAzienda,Logout,UsersList,CreateUserXAzienda,\
+                        RemoteautocompleteAzienda #,GetCantieri
 
 
 urlpatterns = [
         
         #path(r'',IndexHome.as_view()),
-        #path(r'cantieri',GetCantieri.as_view()),
+        path(r'remote',RemoteautocompleteAzienda.as_view()),
         path(r'login',Login.as_view()),
         path(r'logout',Logout.as_view()),
         path(r'auth',Authenticate.as_view()),
-        #path(r'auth',Authenticate.as_view()),
+        path(r'list',UsersList.as_view()),
+        path(r'nuovoutente',CreateUserXAzienda.as_view()),
         path(r'selectazienda/<int:azienda_id>',SelectAzienda.as_view()),
         
 
--- access/views.py
+++ access/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic  import View
+from django.views.generic  import View,ListView,CreateView
 from django.template import loader
 from django.template import Template, Context
-from django.http import HttpResponse,HttpResponseRedirect
-from home.models import Cantiere,Azienda,Cliente,UsersAzienda
+from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.models import User, Group
+
+from home.models import Cantiere,Azienda,Cliente,UsersAzienda,Azienda
+from home.form_utenti import FormUtenti
 
 """
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -16,6 +19,39 @@ from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+
+class RemoteautocompleteAzienda(View):
+    def get(self,request):
+        q = request.GET.get('q')
+        resp=[]
+        res = Azienda.objects.filter(nome__icontains=q).values_list('nome')
+        for one in res:
+            a={}
+            a['nome']=one
+            resp.append(a)
+
+
+        return JsonResponse(resp,safe=False)
+
+
+class UsersList(ListView):
+    model = Azienda
+    template_name = "usersList.html"
+    def get(self,request):
+        u = User.objects.filter(groups__name='Aziende')
+        ua=[]
+
+        #for one in u:
+        #    one.userazienda.all()
+        #    ua.append(one.azienda)
+
+
+
+        context={"users":u}
+        template = loader.get_template(self.template_name)
+        return HttpResponse(template.render(context, request))
+
+
 
 class Authenticate(View):
     def post(self,request):
@@ -52,6 +88,52 @@ class Authenticate(View):
             #return HttpResponse(template.render(context, request))
         else:
             return HttpResponseRedirect('/access/nouserenabled')
+
+
+class CreateUserXAzienda(CreateView):
+    template_name='createuser.html'
+    model = User
+
+    def get(self,request):
+        if not request.user.is_authenticated:        
+            return HttpResponseRedirect('/access/login')
+            
+        form = FormUtenti()
+        az= Azienda.objects.all()
+    
+        return render(request, self.template_name, {'form': form,'aziende':az})
+     
+
+    def post(self,request):
+
+        res = request.POST
+        #az = request.POST('azienda')
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        last_name = request.POST.get('last_name')
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+
+        return JsonResponse(res,safe=False)
+
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        group_azienda = Group.objects.get(name="Azienda")
+        user.groups.add(group_azienda)
+        user.save()
+        
+        #res['az'] = az
+        #for one in az:
+        #a = Azienda.objects.get(id=one)
+        #ua = UsersAzienda(azienda=a,user=user)
+        #ua.save()
+        res2={}
+        res2['q']=res
+        res2['a']=az
+    
+        return JsonResponse(res2,safe=False)
+
 
 class SelectAzienda(View):
     def get(self,request,azienda_id)  :
